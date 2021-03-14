457b4fba17cfe664a023a8dab2115cd35e365189
JimmyVanetta <j_vanetta@zoominternet.net>
2021-03-14 15:00:04 -0400

    adding js to hide edit and complete links on completed tasks

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// hide edit and and complete if task has been marked completed
+
+document.addEventListener("DOMContentLoaded", function () {
+    var checkedList = document.getElementsByClassName('iscompleted');
+    var elements = document.getElementsByClassName('editaction completeaction');
+    var i;
+
+    for (i = 0; i < checkedList.length; i++) {
+        if (checkedList[i].checked) {
+            elements[i].style.display = 'none';
+        }
+    }
+});
