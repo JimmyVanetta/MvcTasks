e265d2ce1cd05b423b2255992d28ace9ca3cca8b
JimmyVanetta <j_vanetta@zoominternet.net>
2021-03-14 23:05:37 -0400

    branching to build api version

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,17 +2,34 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-// hide edit and and complete if task has been marked completed
-
+// API Call
+const callApi = async (callback, controllerName) => {
+    const respone = await fetch(controllerName);
+    const myJson = await respone.json();
+    callback(myJson);
+}
+// hide edit and and complete if task has been marked complete
 document.addEventListener("DOMContentLoaded", function () {
     var checkedList = document.getElementsByClassName('iscompleted');
-    var elements = document.getElementsByClassName('editaction completeaction');
+    var edit = document.getElementsByClassName('editaction');
+    var complete = document.getElementsByClassName('completeaction');
     var i;
 
     for (i = 0; i < checkedList.length; i++) {
-        if (checkedList[i].checked) {
-            elements[i].style.display = 'none';
+        if (checkedList[i].firstElementChild.checked) {
+            edit[i].style.display = 'none';
+            complete[i].style.display = 'none';
         }
     }
+
+    var buildTable = function (jsonData) {
+        var table = document.getElementsByClassName("table")
+        for (var tableData in jsonData) {
+
+        }
+    }
+    if (document.getElementsByClassName("table")) {
+        callApi(buildTable, 'https://localhost:44316/api/tasksapi%27);
+    }
 });
+
