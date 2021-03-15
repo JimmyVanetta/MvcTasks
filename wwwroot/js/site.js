674c2aedcc6cd3d5912403c652102330918e4a0c
JimmyVanetta <j_vanetta@zoominternet.net>
2021-03-15 00:21:14 -0400

    building task cards in js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -8,28 +8,37 @@ const callApi = async (callback, controllerName) => {
     const myJson = await respone.json();
     callback(myJson);
 }
-// hide edit and and complete if task has been marked complete
+
+const buildTaskTable = function () {
+    var buildTable = function (jsonData) {
+        var container = document.getElementById("taskcards");
+        for (var containerData in jsonData) {
+            var card = document.createElement("div");
+            card.style.class = 'card-body';
+            var h5 = document.createElement("h5");
+            var p = document.createElement("p");
+
+            h5.innerText = 'Task';
+            p.innerText = 'Do some tasky things.';
+
+            container.appendChild(card);
+        }
+    }
+    callApi(buildTable, 'https://localhost:44316/api/tasksapi%27');
+}
+
 document.addEventListener("DOMContentLoaded", function () {
     var checkedList = document.getElementsByClassName('iscompleted');
     var edit = document.getElementsByClassName('editaction');
     var complete = document.getElementsByClassName('completeaction');
     var i;
-
+    // Hide edit and complete buttons for completed tasks
     for (i = 0; i < checkedList.length; i++) {
         if (checkedList[i].firstElementChild.checked) {
             edit[i].style.display = 'none';
             complete[i].style.display = 'none';
         }
     }
-
-    var buildTable = function (jsonData) {
-        var table = document.getElementsByClassName("table")
-        for (var tableData in jsonData) {
-
-        }
-    }
-    if (document.getElementsByClassName("table")) {
-        callApi(buildTable, 'https://localhost:44316/api/tasksapi%27');
-    }
+    buildTaskTable();
 });
 
